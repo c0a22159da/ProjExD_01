eeadefd48a59b2a5ce74b66c028c604a4ddc70d5
isayama <c0a22159da@edu.teu.ac.jp>
2023-09-26 15:23:14 +0900

    練習2

--- pygame_image.py
+++ pygame_image.py
@@ -9,11 +9,16 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     tmr = 0
+
+    kk_image = pg.image.load("ex01/fig/3.png")
+    kk_image = pg.transform.flip(kk_image, True, False)
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_image,[0, 0])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
