8f3dfb4751d6a6ec61f0a596562e1c3e14ee1c3e
isayama <c0a22159da@edu.teu.ac.jp>
2023-09-26 15:14:06 +0900

    練習1

--- pygame_image.py
+++ pygame_image.py
@@ -4,8 +4,8 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((1600, 900))
-    
+    screen = pg.display.set_mode((800, 600))
+
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     tmr = 0
